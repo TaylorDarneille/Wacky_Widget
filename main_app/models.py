0599daf5fc52a44dddd0823771190063cb252395
Terrell Lemons <lemonsterrell43@gmail.com>
2020-05-12 18:21:48 -0700

    Added item.form.html / description input

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class item(models.Model):
+class Item(models.Model):
   description = models.CharField(max_length=100)
-  
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Item
+from django.views.generic.edit import CreateView
 
 # Define the index view
 def index(request):
+  item = Item.objects.all()
   return render(request, 'index.html')
+
+class ItemCreate(CreateView):
+  model = Item
+  fields = '__all__' 
+  success_url = '/' 
