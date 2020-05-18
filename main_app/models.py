c695bd3c965f5ed69cbd195cebe0d0407ff2b7ab
Terrell D Lemons <lemonsterrell43@gmail.com>
2020-05-17 19:18:32 -0700

    Add Wire Frame and CSS
    Changes after Completion of Functionality

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,4 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
   description = models.CharField(max_length=100)
-
-class Quantity(models.Model):
-  quantity = models.IntegerField(max_length=10)
+  quantity = models.IntegerField(default=0)
